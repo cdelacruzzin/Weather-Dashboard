c0f1b27c42ab0d87f4204735f124ed45c205a83a
CarlosDelaCruz18 <carlos18@my.yorku.ca>
2023-05-24 16:17:39 -0400

    added jquery for aside

---
+++ script.js
@@ -0,0 +1,8 @@
+var searchBar = $('#searchBar');
+var searchBtn = $('searchBtn');
+
+var curCity = $('curCity');
+var curDate = $('curDate');
+var curTemp = $('curTemp');
+var curwind = $('curwind');
+var curHumidity = $('curHumidity');
