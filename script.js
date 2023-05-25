37f4e027f904b90e7ca0f262b425b92833d8f8ae
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-05-25 15:42:33 -0400

    Added code to calculate the current date and generate a date range for the next 5 days using Day.js

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ var inputCity = "";
 var history = {
 }
 
+//2022-08-30 15:00:00
+var currentDate = dayjs().format('YYYY-MM-DD');
+var startDate = dayjs(currentDate);
+var endDate = startDate.add(5, 'day');
+var dateRange =[];
+
+for (let i = 0; i <= 5; i++) {
+    dateRange.push(startDate.add(i, 'day').format('YYYY-MM-DD'));
+  }
+
+console.log(currentDate);
+console.log(dateRange);
+
 //takes the city name as an argument
 // dynamically creates a new property for the history object 
 // the key will be the city name, the value will be a button
@@ -26,11 +39,11 @@ function appendHistory(city) {
 
 
 //gets the longitude and latitude of the inputted city
-    var API_KEY = 'd4c5beab8e001bfce3529767a56a26ea';
-    var longitude;
-    var latitude;
+    var API_key = 'd4c5beab8e001bfce3529767a56a26ea';
+    var lon;
+    var lat;
 function searchLonLan(city_name) {
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`
 
     fetch(url)
         .then(response => {
@@ -40,22 +53,25 @@ function searchLonLan(city_name) {
             return response.json();
         })
         .then(data => {
-            latitude = data.coord.lat; //stores logitude/latitude from data to corresponding variables
-            longitude = data.coord.lon
+            lat = data.coord.lat; //stores logitude/latitude from data to corresponding variables
+            lon = data.coord.lon;
 
-            fetch(`https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&cnt=5`)
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}`)
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('error: ' + response.status);
+                    throw new Error('error: ' + response.status); //throws an error if city does not exist
                 }
                 return response.json();
             })
             .then(data => {
                 console.log(data.list);
             })
-            appendHistory(city_name);
         })
 }
+
+
+//uses dayjs to get current date
+
 
 
 
