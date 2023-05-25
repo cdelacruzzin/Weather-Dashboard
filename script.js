1987aa46c50b5fa561e8cacc289744e46cd641e7
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-05-25 11:53:55 -0400

    history object where the keys will be the city name, the value will be a function

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 //have queries linking search bar, search btn.
 //when button is clicked, the value of the search bar is stored to a 'inputCity' variable
 // add buttons dynamically to display a history of searched cities
-//
-
-
-
-
-
-
 var searchBtn = $('#searchBtn');
 var searchBar = $('#searchBar');
+var historyContainer = $('#history');
 var inputCity ="";
 
 
+
+
+
+
+
+
+
+
+// will create a history container
+// elements will be stored in an object
+//the key will be the city name, the value will be a function
+
+var history = {
+
+}
+
+
+function appendHistory(city){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// onClick function that stores the value of the search bar when clicked
 searchBtn.on('click', function(event) {
     event.preventDefault();
     inputCity = searchBar.val();
-    console.log(inputCity);
-})
+    appendHistory();
+});
