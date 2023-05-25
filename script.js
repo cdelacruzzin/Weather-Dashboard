156b137abc4fb4335b2ba8a363b94f1b8f5fb29f
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-05-25 12:50:38 -0400

    finished appedHistory(city).

--- script.js
+++ script.js
@@ -18,25 +18,24 @@ var inputCity ="";
 // will create a history container
 // elements will be stored in an object
 //the key will be the city name, the value will be a function
-
 var history = {
-    //Austin: searchWeather('Austin')
 }
 
 //takes the city name as an argument
 // dynamically creates a new property for the history object 
-// the key will be the city name, the value will be searchWeather()
-//creates a button element with the city name as its text
+// the key will be the city name, the value will be a button
+//creates a button element with the city name as its text and value, and a class of 'historyBtns'
 //all buttons will be in the same class to share an onClick functionality
 function appendHistory(city){
-//history.${city} = searchWeather('${city}')
-//history.Austin = searchWeather('Austin')
-//creates buttonEL
-//adds a class that all appended buttons will have
-//buttonEL.text = ${city}
-// button will look like : <button class="sameclass" onClick="searchWeather(${city}">
-//appends this button to historyContainer
+var btnName = $('<button>');
+btnName.text(city);
+btnName.val(city);
+btnName.addClass('historyBns');
 
+historyContainer.append(btnName);
+
+history[city] = btnName;
+console.log(history);
 }
 
 
@@ -48,18 +47,9 @@ function searchWeather(city) {
 
 
 
-function hi() {
-    console.log('ddddddd');
-}
-
-
-
-
-
-
 // onClick function that stores the value of the search bar when clicked
 searchBtn.on('click', function(event) {
     event.preventDefault();
     inputCity = searchBar.val();
-    appendHistory();
+    appendHistory(inputCity);
 });
