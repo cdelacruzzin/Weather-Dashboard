fefbb1770744ec8d63aa77dfb644ba6e87bf2f82
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-05-25 17:03:45 -0400

    fixed for loop in generateDateRange()

--- script.js
+++ script.js
@@ -13,15 +13,17 @@ var history = {
 var currentDate = dayjs().format('YYYY-MM-DD'); // Get the current date and format it as 'YYYY-MM-DD'
 var startDate = dayjs(currentDate); // Set the current date as the start date
 var endDate = startDate.add(5, 'day');// Add 5 days to the start date to get the end date
+var numberOfDays = 5;
 var dateRange =[];
 
 
   
 
   function generateDateRange(startDate, numberOfDays) {
-    for (let i = 0; i < numberOfDays; i++) {
+    for (let i = 0; i <= numberOfDays; i++) {
       dateRange.push(startDate.add(i, 'day').format('YYYY-MM-DD'));
     }
+    console.log(dateRange);
   }
 
 //takes the city name as an argument
@@ -70,13 +72,6 @@ function searchLonLan(city_name) {
             })
         })
 }
-
-
-//uses dayjs to get current date
-
-
-
-
 
 
 generateDateRange(startDate, numberOfDays);
